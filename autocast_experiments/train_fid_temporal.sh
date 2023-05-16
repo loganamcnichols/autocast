edbd7d2ef4b5c833681a60318c0e831844a6255b
loganamcnichols <loganamcnichols@gmail.com>
2023-05-16 08:11:56 +0000

    Updated .sh file.

--- autocast_experiments/train_fid_temporal.sh
+++ autocast_experiments/train_fid_temporal.sh
@@ -1,54 +1,25 @@
 #!/bin/sh
-
-export READER=t5
-export MODELSIZE=3b
-export TOPN=2
-
-export seqlen=128 # maximum lookback days
-export finetune_encoder=0 # freeze FiD Static model
-export adjust_targets=1 # improve crowd forecasts with true resolution
-
-export BSZ=8
-export EPOCHS=5
-export OPTIMTYPE=adamw
-export SCHEDULERTYPE=fixed
-export WDECAY=1e-2
-export LR=5e-5
-export TRAINSIZE=4387 # number of train examples
-export WARMUP=100
-
-export RETR=bm25ce
-export TEST_QUESTIONS=data/test_questions.json
-export TRAIN_QUESTIONS=data/train_questions.json
-export TRAIN_CROWD=data/train_crowd.json
-export TEST_CROWD=data/test_crowd.json
-export TRAIN_SCHEDULE=data/train_schedule.json
-export TEST_SCHEDULE=data/test_schedule.json
-
-# export LOAD=t5_${MODELSIZE}_top10_linear_wdecay1e-2_lr5e-5_bs8_ep10_retrbm25ce # load FiD Static model
-
 python train_fid_temporal.py \
-        --max_seq_len $seqlen \
-        --finetune_encoder $finetune_encoder \
-        --adjust_targets $adjust_targets \
-        --model_size $MODELSIZE \
-        --per_gpu_batch_size $BSZ \
-        --epochs $EPOCHS \
-        --answer_maxlength 15 \
-        --text_maxlength 512 \
-        --train_questions $TRAIN_QUESTIONS \
-        --test_questions $TEST_QUESTIONS \
-        --train_crowd $TRAIN_CROWD \
-        --test_crowd $TEST_CROWD \
-        --train_schedule $TRAIN_SCHEDULE \
-        --test_schedule $TEST_SCHEDULE \
-        --n_context $TOPN \
-        --name temporal_${READER}_${MODELSIZE}_top${TOPN}_seqlen${seqlen}_${SCHEDULERTYPE}_wdecay${WDECAY}_lr${LR}_bs${BSZ}_ep${EPOCHS}_retr${RETR}_finetune${finetune_encoder}_adjusttarget${adjust_targets} \
-        --optim $OPTIMTYPE \
-        --lr $LR \
-        --weight_decay $WDECAY \
-        --scheduler $SCHEDULERTYPE \
-        --warmup_steps $WARMUP \
-        --train_data_size $TRAINSIZE \
-        --use_checkpoint \
-        --model_path checkpoint/${LOAD}/checkpoint/epoch-10
+    --max_seq_len 128 \
+    --finetune_encoder 0 \
+    --adjust_targets 1 \
+    --model_size small \
+    --per_gpu_batch_size 8 \
+    --epochs 1 \
+    --answer_maxlength 12 \
+    --text_maxlength 512 \
+    --train_questions data/train_questions.json \
+    --test_questions data/test_questions.json \
+    --train_crowd data/train_crowd.json \
+    --test_crowd data/test_crowd.json \
+    --train_schedule data/train_reading.json \
+    --test_schedule data/test_reading.json \
+    --n_context 2 \
+    --name temporal_t5_3b_top2_seqlen128_fixed_wdecay1e-2_lr5e-5_bs8_ep5_retrbm25ce_finetune0_adjusttarget1 \
+    --optim adamw \
+    --lr 5e-5 \
+    --weight_decay 1e-2 \
+    --scheduler fixed \
+    --warmup_steps 100 \
+    --train_data_size 4387
+
